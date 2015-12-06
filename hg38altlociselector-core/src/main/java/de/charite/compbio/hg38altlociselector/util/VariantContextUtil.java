d6a318444c520c6ac9cf6ecc51b40381a3e4a188
Marten Jäger <martenjaeger@gmx.de>
2015-12-06 15:49:37 +0100

    minor

--- hg38altlociselector-core/src/main/java/de/charite/compbio/hg38altlociselector/util/VariantContextUtil.java
+++ hg38altlociselector-core/src/main/java/de/charite/compbio/hg38altlociselector/util/VariantContextUtil.java
@@ -3,10 +3,15 @@
  */
 package de.charite.compbio.hg38altlociselector.util;
 
+
+import htsjdk.variant.variantcontext.VariantContext;
+
 /**
+ * Collection of tools to perform on {@link VariantContext} collections.
  * @author Marten Jäger <marten.jaeger@charite.de>
  *
  */
 public class VariantContextUtil {
+	
 
 }
