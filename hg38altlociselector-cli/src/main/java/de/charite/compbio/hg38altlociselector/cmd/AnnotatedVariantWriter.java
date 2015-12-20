31a1699afdaf3ca8c39918f39af08c02e1565d1b
martenj <martenjaeger@gmx.de>
2015-12-20 02:52:35 +0100

    added VaraintWriter, Exception, TopLevelChromosomes

---
+++ hg38altlociselector-cli/src/main/java/de/charite/compbio/hg38altlociselector/cmd/AnnotatedVariantWriter.java
@@ -0,0 +1,72 @@
+package de.charite.compbio.hg38altlociselector.cmd;
+
+import java.io.IOException;
+import java.util.EnumSet;
+
+import de.charite.compbio.hg38altlociselector.Hg38altLociSeletorOptions;
+import de.charite.compbio.hg38altlociselector.exceptions.AnnotationException;
+import htsjdk.samtools.reference.ReferenceSequenceFile;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.writer.Options;
+import htsjdk.variant.variantcontext.writer.VariantContextWriter;
+import htsjdk.variant.variantcontext.writer.VariantContextWriterBuilder;
+import htsjdk.variant.vcf.VCFFileReader;
+import htsjdk.variant.vcf.VCFFilterHeaderLine;
+import htsjdk.variant.vcf.VCFHeader;
+import htsjdk.variant.vcf.VCFHeaderLine;
+
+/**
+ * Writer for the
+ *
+ * @author Marten Jäger <marten.jaeger@charite.de>
+ */
+public class AnnotatedVariantWriter {
+    /** writer for annotated VariantContext objects */
+    private final VariantContextWriter out;
+
+    public AnnotatedVariantWriter(VCFFileReader reader, ReferenceSequenceFile refFile,
+            Hg38altLociSeletorOptions options) {
+        VariantContextWriterBuilder builder = new VariantContextWriterBuilder()
+                .setReferenceDictionary(refFile.getSequenceDictionary())
+                .setOptions(EnumSet.of(Options.INDEX_ON_THE_FLY)).setOutputFile(options.outputVcf);
+        if (refFile.getSequenceDictionary() == null)
+            builder.unsetOption(Options.INDEX_ON_THE_FLY);
+        this.out = builder.build();
+
+        VCFHeader header = new VCFHeader(reader.getFileHeader());
+        // header.setSequenceDictionary(refFile.getSequenceDictionary());
+        header.addMetaDataLine(new VCFHeaderLine("hg38altLociSelectorVersion", options.VERSION));
+        header.addMetaDataLine(new VCFHeaderLine("hg38altLociSelectorCommand", "TODO ..."));
+        // TODO add line with Commadn line call
+        header.addMetaDataLine(
+                new VCFFilterHeaderLine("ALTLOCI", "Filtered due to a more likely alternative scaffold"));
+        this.out.writeHeader(header);
+    }
+
+    /**
+     * Write out the given VariantContext with additional annotation.
+     *
+     * @throws AnnotationException
+     *             when a problem with annotation occurs
+     * @throws IOException
+     *             when problem with I/O occurs
+     */
+    // public void put(VariantContext vc) throws AnnotationException,
+    // IOException {
+    public void put(VariantContext vc) {
+        // try {
+        // out.add(vc);
+        // } catch (TribbleException e) {
+        // e.printStackTrace();
+        // }
+        out.add(vc);
+    }
+
+    /**
+     * Close writer, free resources
+     */
+    public void close() {
+        out.close();
+    }
+
+}
