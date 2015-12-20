e8972e0ee661d42ed450cbd8f8dbcd416aa6327d
martenj <martenjaeger@gmx.de>
2015-12-20 02:53:49 +0100

    added output of the Varaints to file

--- hg38altlociselector-cli/src/main/java/de/charite/compbio/hg38altlociselector/cmd/AnnotateVCFCommand.java
+++ hg38altlociselector-cli/src/main/java/de/charite/compbio/hg38altlociselector/cmd/AnnotateVCFCommand.java
@@ -19,7 +19,11 @@ import de.charite.compbio.hg38altlociselector.data.RegionBuilder;
 import de.charite.compbio.hg38altlociselector.exceptions.AltLociSelectorException;
 import de.charite.compbio.hg38altlociselector.exceptions.CommandLineParsingException;
 import de.charite.compbio.hg38altlociselector.exceptions.HelpRequestedException;
+import de.charite.compbio.hg38altlociselector.reference.TopLevelChromosomes;
 import de.charite.compbio.hg38altlociselector.util.VariantContextUtil;
+import htsjdk.samtools.reference.ReferenceSequenceFile;
+import htsjdk.samtools.reference.ReferenceSequenceFileFactory;
+import htsjdk.samtools.util.CloseableIterator;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFFileReader;
 
@@ -36,7 +40,8 @@ import htsjdk.variant.vcf.VCFFileReader;
  * <li>select region
  * <li>pick all alt loci for this region
  * <li>get all variants, that are in this region
- * <li>compare the overlaps and decide which allele (ref, alt.loci) is the most probable
+ * <li>compare the overlaps and decide which allele (ref, alt.loci) is the most
+ * probable
  * </ol>
  * 
  *
@@ -45,98 +50,170 @@ import htsjdk.variant.vcf.VCFFileReader;
  */
 public class AnnotateVCFCommand extends AltLociSelectorCommand {
 
-	/**
-	 * @param args
-	 * @throws HelpRequestedException
-	 * @throws CommandLineParsingException
-	 */
-	public AnnotateVCFCommand(String[] args) throws CommandLineParsingException, HelpRequestedException {
-		super(args);
-	}
+    /**
+     * @param args
+     * @throws HelpRequestedException
+     * @throws CommandLineParsingException
+     */
+    public AnnotateVCFCommand(String[] args) throws CommandLineParsingException, HelpRequestedException {
+        super(args);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.charite.compbio.hg38altlociselector.cmd.AltLociSelectorCommand# parseCommandLine(java.lang.String[])
-	 */
-	@Override
-	protected Hg38altLociSeletorOptions parseCommandLine(String[] args)
-			throws CommandLineParsingException, HelpRequestedException {
-		try {
-			return new AnnotateVCFCommandLineParser().parse(args);
-		} catch (ParseException e) {
-			throw new CommandLineParsingException("Could not parse the command line.", e);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.charite.compbio.hg38altlociselector.cmd.AltLociSelectorCommand#
+     * parseCommandLine(java.lang.String[])
+     */
+    @Override
+    protected Hg38altLociSeletorOptions parseCommandLine(String[] args)
+            throws CommandLineParsingException, HelpRequestedException {
+        try {
+            return new AnnotateVCFCommandLineParser().parse(args);
+        } catch (ParseException e) {
+            throw new CommandLineParsingException("Could not parse the command line.", e);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.charite.compbio.hg38altlociselector.cmd.AltLociSelectorCommand#run()
-	 */
-	@Override
-	public void run() throws AltLociSelectorException {
-		// alt. loci info parsing
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.charite.compbio.hg38altlociselector.cmd.AltLociSelectorCommand#run()
+     */
+    @Override
+    public void run() throws AltLociSelectorException {
+        // init Variant INPUT
+        final VCFFileReader inputVCF = new VCFFileReader(new File(this.options.inputVcf));
 
-		VCFFileReader inputVCF = new VCFFileReader(new File(this.options.inputVcf));
+        // init regions with alt. scaffolds
+        ImmutableList<Region> regions = new RegionBuilder(options.altAccessionsPath, options.altScaffoldPlacementPath,
+                options.genomicRegionsDefinitionsPath, options.chrAccessionsPath).build();
+                // System.out.println(regions.size());
 
-		ImmutableList<Region> regions = new RegionBuilder(options.altAccessionsPath, options.altScaffoldPlacementPath,
-				options.genomicRegionsDefinitionsPath, options.chrAccessionsPath).build();
-		System.out.println(regions.size());
-		int c = 0;
-		for (Region region : regions) {
-			if (region.getLoci() == null) {
-				System.out.println("[INFO] skip region: " + region.getRegionInfo().getRegionName());
-				continue;
-			}
-			System.out.println(
-					"[INFO] region: " + region.getRegionInfo().getRegionName() + ": " + region.getLoci().size());
-			ArrayList<VariantContext> refVariantList = Lists
-					.newArrayList(inputVCF.query("chr" + region.getRegionInfo().getChromosomeInfo().getChromosome(),
-							region.getRegionInfo().getStart(), region.getRegionInfo().getStop()));
+        // init Reference FastA file e.g. Sequence Dictionary
+        final ReferenceSequenceFile refFile = ReferenceSequenceFileFactory
+                .getReferenceSequenceFile(new File(options.referencePath));
+        if (!refFile.isIndexed()) {
+            System.err.println("Reference fasta file is not indexed");
+            System.exit(1); // TODO throw exception
+        }
 
-			ArrayList<PairwiseVariantContextIntersect> intersectList = new ArrayList<>();
-			for (MetaLocus locus : region.getLoci().values()) {
-				VCFFileReader locusVCF = new VCFFileReader(new File(
-						this.options.tempFolder + "/" + locus.getAccessionInfo().createFastaIdentifier() + ".vcf.gz"));
-				ArrayList<VariantContext> locusVariantList = Lists.newArrayList(locusVCF.iterator());
-				PairwiseVariantContextIntersect inter = VariantContextUtil.intersectVariantContext(refVariantList,
-						locusVariantList);
-				System.out.println(inter.toString());
-				// intersectList.add(VariantContextUtil.intersectVariantContext(refVariantList, locusVariantList));
-			}
-			// System.exit(0);
-			c += region.getLoci().size();
-		}
-		System.out.println("total of loci: " + c);
+        // init Variant file writer
+        // final VariantContextWriter writerVCF = new
+        // VariantContextWriterBuilder()
+        // .setReferenceDictionary(refFile.getSequenceDictionary())
+        // .setOptions(EnumSet.of(Options.INDEX_ON_THE_FLY)).setOutputFile(options.outputVcf).build();
+        final AnnotatedVariantWriter writerVCF = new AnnotatedVariantWriter(inputVCF, refFile, options);
 
-		// ------------------------------------------------------------------------------------- //
+        // start writing
 
-		// ImmutableList<AlternativeLocus> loci = new AlternativeLociBuilder(options.altAccessionsPath,
-		// options.altScaffoldPlacementPath, options.genomicRegionsDefinitionsPath).build();
+        int regionIdx = 0;
+        // int chrStart;
 
-		// VariantContextComparator variantContextComparator = null;
-		// VCFFileReader reader = new VCFFileReader(new File(this.options.inputVcf));
-		// // System.out.println(reader.getFileHeader().toString());
-		// VCFFileReader altLoci = new VCFFileReader(new File(this.options.altlociVcf));
-		// final Collection<CloseableIterator<VariantContext>> iteratorCollection = new
-		// ArrayList<CloseableIterator<VariantContext>>(
-		// 2);
-		// iteratorCollection.add(reader.iterator());
-		// iteratorCollection.add(altLoci.iterator());
-		// VCFHeader header = reader.getFileHeader();
-		// header.addMetaDataLine(new VCFContigHeaderLine("<assembly=hg38>", VCFHeaderVersion.VCF4_1, "chr1", 0));
-		// variantContextComparator = reader.getFileHeader().getVCFRecordComparator();
-		// System.out.println(reader.getFileHeader().toString());
-		// // if
-		// // (!variantContextComparator.isCompatible(reader.getFileHeader().getContigLines()))
-		// // {
-		// // System.err.println("sample VCF file not compatible with comparator");
-		// // }
-		//
-		// final MergingIterator<VariantContext> mergingIterator = new MergingIterator<VariantContext>(
-		// variantContextComparator, iteratorCollection);
+        CloseableIterator<VariantContext> currentVariants;
 
-	}
+        // currentVariants = inputVCF.iterator();
+        // while (currentVariants.hasNext()) {
+        // try {
+        // writerVCF.put(currentVariants.next());
+        // } catch (TribbleException e) {
+        // e.printStackTrace();
+        // }
+        // // writerVCF.put(currentVariants.next());
+        // }
+        // writerVCF.close();
+        // System.out.println("finished");
+        // System.exit(0);
+        // TODO find a better way to perform region selection - maybe with the
+        // database
+        for (String chr : TopLevelChromosomes.getInstance().getToplevel()) {
+            // chrStart = 0;
+            ArrayList<Integer> regionsOnChromosome = new ArrayList<>();
+            for (int i = 0; i < regions.size(); i++) {
+                if (regions.get(i).getRegionInfo().getChromosomeInfo().getChromosome().compareTo(chr) == 0)
+                    regionsOnChromosome.add(i);
+            }
+            if (regionsOnChromosome.isEmpty()) {
+                System.out.println(chr + " w/o region(s)");
+                writeVariants(inputVCF, refFile, writerVCF, chr, 1, refFile.getSequence("chr" + chr).length());
+            } else {
+                // writeVariants(inputVCF, refFile, writerVCF, chr, 1,
+                // regions.get(regionsOnChromosome.get(0)).getRegionInfo().getStart()
+                // - 1);
+                int i;
+                for (i = 0; i < regionsOnChromosome.size(); i++) {
+                    // write out block before region
+                    if (i == 0)
+                        writeVariants(inputVCF, refFile, writerVCF, chr, 1,
+                                regions.get(regionsOnChromosome.get(i)).getRegionInfo().getStart() - 1);
+                    else
+                        writeVariants(inputVCF, refFile, writerVCF, chr,
+                                regions.get(regionsOnChromosome.get(i - 1)).getRegionInfo().getStop() + 1,
+                                regions.get(regionsOnChromosome.get(i)).getRegionInfo().getStart() - 1);
+                    // TODO check with unittesting if the STart AND stop are
+                    // including
+
+                }
+                // write final block up to the end of the chromosome
+                writeVariants(inputVCF, refFile, writerVCF, chr,
+                        regions.get(regionsOnChromosome.get(i - 1)).getRegionInfo().getStop() + 1,
+                        refFile.getSequence("chr" + chr).length());
+
+                System.out.println(chr + " - " + regionsOnChromosome);
+            }
+        }
+
+        // close the variant writer
+        writerVCF.close();
+
+        System.exit(0);
+
+        int c = 0;
+        for (Region region : regions) {
+            if (region.getLoci() == null) {
+                System.out.println("[INFO] skip region: " + region.getRegionInfo().getRegionName());
+                continue;
+            }
+            System.out.println(
+                    "[INFO] region: " + region.getRegionInfo().getRegionName() + ": " + region.getLoci().size());
+            ArrayList<VariantContext> refVariantList = Lists
+                    .newArrayList(inputVCF.query("chr" + region.getRegionInfo().getChromosomeInfo().getChromosome(),
+                            region.getRegionInfo().getStart(), region.getRegionInfo().getStop()));
+
+            ArrayList<PairwiseVariantContextIntersect> intersectList = new ArrayList<>();
+            for (MetaLocus locus : region.getLoci().values()) {
+                VCFFileReader locusVCF = new VCFFileReader(new File(
+                        this.options.tempFolder + "/" + locus.getAccessionInfo().createFastaIdentifier() + ".vcf.gz"));
+                ArrayList<VariantContext> locusVariantList = Lists.newArrayList(locusVCF.iterator());
+                PairwiseVariantContextIntersect inter = VariantContextUtil.intersectVariantContext(refVariantList,
+                        locusVariantList);
+                System.out.println(inter.toString());
+                // intersectList.add(VariantContextUtil.intersectVariantContext(refVariantList,
+                // locusVariantList));
+            }
+            // System.exit(0);
+            c += region.getLoci().size();
+        }
+        System.out.println("total of loci: " + c);
+
+    }
+
+    /**
+     * Writes the Variants out
+     * 
+     * @param reader
+     * @param refFile
+     * @param writer
+     * @param chr
+     */
+    private void writeVariants(VCFFileReader reader, ReferenceSequenceFile refFile, AnnotatedVariantWriter writer,
+            String chr, int start, int stop) {
+
+        CloseableIterator<VariantContext> currentVariants = reader.query("chr" + chr, start, stop);
+        while (currentVariants.hasNext()) {
+            writer.put(currentVariants.next());
+        }
+    }
 
 }
