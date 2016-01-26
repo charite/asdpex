13373bc6df8b43707d30a0b3c936f49c538b54b0
Marten Jaeger <marten.jaeger@charite.de>
2016-01-26 13:37:31 +0100

    added skipt to filter ASPDs from badly aligned regions

--- scripts/fixVCFs.pl
+++ scripts/fixVCFs.pl
@@ -1,10 +1,27 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
+use Getopt::Std;
+use Getopt::Long;
 use IO::Zlib;
 
-my $infile="/media/mjaeger/USB-Stick/vcf/1008.HS38.aln.normalized.vcf.gz";
-my $outfile="/home/mjaeger/git/hg38altLociSelector/vcf/1008.HS38.aln.normalized.fixed.vcf.gz";
+my $infile;
+my $outfile;
+my $help;
+
+GetOptions(	'vcfin|i=s'	=> \$infile,
+#		'vcfout|o=s'	=> \$outfile,
+		'help|h'	=> \$help);
+
+if($help){printUsage()}
+if(!$infile){printUsage()}
+#if(!$outfile){printUsage()}
+
+
+#my $infile="/media/mjaeger/USB-Stick/vcf/1008.HS38.aln.normalized.vcf.gz";
+
+#my $outfile="/home/mjaeger/git/hg38altLociSelector/vcf/1008.HS38.aln.normalized.fixed.vcf.gz";
 
 tie *IN, 'IO::Zlib', $infile, "rb";
 #tie *OUT, 'IO::Zlib', $outfile, "wb";
@@ -19,3 +36,32 @@ while (<IN>) {
 	    print $line;
      }
 };
+
+close(IN);
+#close(OUT);
+
+sub printUsage{
+	print while(<DATA>);
+	exit;
+}
+
+
+__DATA__
+
+
+	fixVCFs.pl
+
+SYNOPSIS
+
+	perl fixVCFs.pl -i inputfile.vcf
+
+DESCRIPTION
+
+	This will fix the output from Freebayes which makes it incompatible with HTSJDK/HTSLIB.
+
+	fixVCFs.pl [OPTIONS]
+
+OPTIONS:
+	-i	--vcfin		<path to VCF file>
+	-o	--vcfout		<path to fixed VCF file>
+	-h	--help
