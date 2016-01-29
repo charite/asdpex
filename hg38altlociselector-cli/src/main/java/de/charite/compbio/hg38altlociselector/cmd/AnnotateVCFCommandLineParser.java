64ea0ea1c04dee2732ebef47bfde395bd3cb69c1
Marten Jaeger <marten.jaeger@charite.de>
2016-01-29 17:42:40 +0100

    added tmp directory option

--- hg38altlociselector-cli/src/main/java/de/charite/compbio/hg38altlociselector/cmd/AnnotateVCFCommandLineParser.java
+++ hg38altlociselector-cli/src/main/java/de/charite/compbio/hg38altlociselector/cmd/AnnotateVCFCommandLineParser.java
@@ -36,6 +36,7 @@ public class AnnotateVCFCommandLineParser {
         options.addOption("a", "alt", true, "VCF file with alt loci information");
         options.addOption("o", "out", true, "VCF output file with the annotations");
         options.addOption("r", "ref", true, "reference fasta file with index");
+        options.addOption(null, "tmp", true, "folder with the temporary files");
         parser = new DefaultParser();
     }
 
@@ -78,6 +79,9 @@ public class AnnotateVCFCommandLineParser {
             result.error = "Missing indexed reference fasta file: -r";
             printHelp(result);
         }
+
+        if (cmd.hasOption("tmp"))
+            result.tempFolder = cmd.getOptionValue("tmp");
 
         return result;
     }
