688a923f297338f0053c94d620a8601b1c4d9203
Marten Jäger <martenjaeger@gmx.de>
2019-12-12 13:36:51 +0100

    more fixed version number

--- asdpex-cli/src/main/java/de/charite/compbio/asdpex/Hg38altLociSeletorOptions.java
+++ asdpex-cli/src/main/java/de/charite/compbio/asdpex/Hg38altLociSeletorOptions.java
@@ -17,7 +17,7 @@ import java.io.File;
  */
 public final class Hg38altLociSeletorOptions {
 
-    public static final String VERSION = "0.2";
+    public static final String VERSION = "0.3";
 
     public static final String VCFALTLOCUSSTRING = "ALTLOCUS";
 
